de3549ffa429bbd7e154040c5d58b4690251ec69
junil3005 <yjl3005@naver.com>
2024-12-27 11:59:36 +0900

    hami project

---
+++ src/component/CreateCode.jsx
@@ -0,0 +1,11 @@
+import QRCode from "qrcode.react";
+
+const CreateCode = ({}: p) => {
+    return (
+        <Container>
+        <QRCode value={QR에 담고 싶은 정보} />
+        </Container>
+    );
+}
+
+export default CreateCode;
